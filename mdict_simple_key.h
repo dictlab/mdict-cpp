3506926addd608126f3c54c4425f9f8141b96782
Konstantin <alexandroviski8299@gmail.com>
2025-06-11 04:34:27 +0000

    Update mdict_simple_key.h

--- mdict_simple_key.h
+++ mdict_simple_key.h
@@ -1,9 +1,12 @@
 #ifndef mdict_simple_key_item_h
 #define mdict_simple_key_item_h
 
+#include <stdint.h>
+
 typedef struct simple_key_list_item {
-    unsigned long record_start;
+    uint64_t record_start; // Supports files >4GB
     char* key_word;
 } simple_key_item;
+
 
 #endif
